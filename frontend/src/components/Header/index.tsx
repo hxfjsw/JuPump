5de217d77ee15bca38ad1c013668bda1443f3943
hxfjsw <hxfjsw@qq.com>
2025-07-20 01:47:22 +0800

    Bridge

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -362,8 +362,8 @@ export default function Header() {
         <StyledNavLink id={`stake-nav-link`} to={'/vote'}>
           Vote
         </StyledNavLink>
-        <StyledExternalLink id={`stake-nav-link`} href={'#'}>
-          Charts <span style={{ fontSize: '11px', textDecoration: 'none !important' }}>↗</span>
+        <StyledExternalLink id={`stake-nav-link`} href={'https://www.juchain.org/en/bridge'}>
+          Bridge <span style={{ fontSize: '11px', textDecoration: 'none !important' }}>↗</span>
         </StyledExternalLink>
       </HeaderLinks>
       <HeaderControls>
