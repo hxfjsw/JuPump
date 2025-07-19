7a1bcba02985d1669fe043c5d33a7155b921ea59
hxfjsw <hxfjsw@qq.com>
2025-07-20 01:00:37 +0800

    JuScan

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -362,7 +362,7 @@ export default function Header() {
         <StyledNavLink id={`stake-nav-link`} to={'/vote'}>
           Vote
         </StyledNavLink>
-        <StyledExternalLink id={`stake-nav-link`} href={'https://info.uniswap.org'}>
+        <StyledExternalLink id={`stake-nav-link`} href={'#'}>
           Charts <span style={{ fontSize: '11px', textDecoration: 'none !important' }}>↗</span>
         </StyledExternalLink>
       </HeaderLinks>
